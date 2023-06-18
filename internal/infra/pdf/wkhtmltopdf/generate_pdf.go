740478dabf0e2c9cf13e77a31ab77a494c1a7ac0
Filipe Augusto Gonçalves <flpgst@gmail.com>
2023-06-17 21:49:17 -0300

    feat: divide template in parts

--- internal/infra/pdf/wkhtmltopdf/generate_pdf.go
+++ internal/infra/pdf/wkhtmltopdf/generate_pdf.go
@@ -18,15 +18,28 @@ func NewWKHTMLTOPDF() *WKHTMLTOPDF {
 }
 
 func (wk *WKHTMLTOPDF) GeneratePDF(dto *dto.ReportDTO) (*os.File, error) {
-	templateAbsPath, err := filepath.Abs("../../internal/infra/pdf/templates/simple_report.html")
+	templateDir, err := filepath.Abs("../../internal/infra/pdf/template")
 	if err != nil {
 		fmt.Println(err)
 		return nil, err
 	}
 
-	tmpl, err := template.ParseFiles(templateAbsPath)
 	if err != nil {
-		fmt.Println("Error parsing template:", err)
+		fmt.Println(err)
+		return nil, err
+	}
+	tmpl := template.New("")
+
+	templateFiles := []string{
+		filepath.Join(templateDir, "template.html"),
+		filepath.Join(templateDir, "header/simple_header.html"),
+		filepath.Join(templateDir, "body/simple_body.html"),
+		filepath.Join(templateDir, "footer/simple_footer.html"),
+	}
+
+	tmpl, err = tmpl.ParseFiles(templateFiles...)
+	if err != nil {
+		fmt.Println("Error parsing template files:", err)
 		return nil, err
 	}
 
@@ -37,7 +50,23 @@ func (wk *WKHTMLTOPDF) GeneratePDF(dto *dto.ReportDTO) (*os.File, error) {
 	}
 	defer os.Remove(htmlOutputFile.Name())
 
-	err = tmpl.Execute(htmlOutputFile, dto)
+	data := struct {
+		ReportName  string
+		Date        string
+		Header      []string
+		Body        []string
+		Footer      []string
+		TemplateDir string
+	}{
+		ReportName:  dto.ReportName,
+		Date:        dto.Date,
+		Header:      dto.Header,
+		Body:        dto.Body,
+		Footer:      dto.Footer,
+		TemplateDir: templateDir,
+	}
+
+	err = tmpl.ExecuteTemplate(htmlOutputFile, "template.html", data)
 	if err != nil {
 		fmt.Println("Error executing template:", err)
 		return nil, err
@@ -50,7 +79,15 @@ func (wk *WKHTMLTOPDF) GeneratePDF(dto *dto.ReportDTO) (*os.File, error) {
 	}
 	defer os.Remove(pdfOutputFile.Name())
 
-	cmd := exec.Command("wkhtmltopdf", htmlOutputFile.Name(), pdfOutputFile.Name())
+	cmd := exec.Command("wkhtmltopdf", "--enable-local-file-access", htmlOutputFile.Name(), pdfOutputFile.Name())
+	errorLogFile, err := os.Create("error.log")
+	if err != nil {
+		fmt.Println("Error creating error log file:", err)
+		return nil, err
+	}
+	defer errorLogFile.Close()
+
+	cmd.Stderr = errorLogFile
 	err = cmd.Run()
 	if err != nil {
 		fmt.Println("Error generating PDF:", err)
